4f62782276757af39dbcee549891f3b48cb08bff
Gilia Gongora <giliagongora.g@gmail.com>
2017-05-16 21:08:11 -0400

    Desarrollo y estructuración de archivos

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+// function de demo, al presionar el boton se ejecuta la funcion
+document.getElementById("demo").addEventListener("click", 
+	function botoncillo(){
+	alert("Hola Mundillo");
+	});
+
+//Se ejecuta cuando se reinicia el site
+function cargando(){
+	alert("Loading site");
+};
+	cargando();
+
+//Se ejecuta cuando se esta sobre el input y lanza el alert
+function over_my(){
+	alert("Estás sobre mi");
+};
+//Se ejecuta cuando se esta sobre el input y lanza el alert
+function out_my(){
+	alert("No estás sobre mi");
+};
+
+
+
